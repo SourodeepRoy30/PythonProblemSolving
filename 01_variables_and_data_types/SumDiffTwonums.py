2970e06fd675d8b980c361944095e387f59040f8
SourodeepR <sourodeeproy1@gmail.com>
2026-05-30 00:00:33 +0530

    first problem

---
+++ 01_variables_and_data_types/SumDiffTwonums.py
@@ -0,0 +1,17 @@
+# Sum and Difference of Two Numbers
+# You are given two integers as input. 
+# Your task is to print four results — their sum, their difference, their product, and their quotient.
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+# Calculate the sum, difference, product, and quotient
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b 
+
+print(sum)
+print(difference)
+print(product)
+print(quotient)
