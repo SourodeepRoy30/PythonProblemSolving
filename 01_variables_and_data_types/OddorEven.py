ed890e9104cdbc9bd1032764c2eb3ce00967ec8d
SourodeepR <sourodeeproy1@gmail.com>
2026-05-31 15:48:56 +0530

    Correction in odd or even

--- 01_variables_and_data_types/OddorEven.py
+++ 01_variables_and_data_types/OddorEven.py
@@ -1,12 +1,16 @@
 # Check if a Number is Even or Odd
 # Given an integer, determine if it is even or odd and print the result.
+# Check if a Number is Even or Odd
 
-number = int(input("Enter an integer: "))
-if number % 2 == 0:
-    print(f"{number} is an even number.")
-elif number % 2 != 0:
-    print(f"{number} is an odd number.")
-elif number == 0:
-    print("0 is neither even nor odd.")
-else:
-    print("Invalid input.")
+try:
+    number = int(input("Enter an integer: "))
+    
+    if number == 0:
+        print("0 is neither even nor odd.")
+    elif number % 2 == 0:
+        print(f"{number} is an even number.")
+    else:
+        print(f"{number} is an odd number.")
+
+except ValueError:
+    print("Invalid input. Please enter a whole number.")
